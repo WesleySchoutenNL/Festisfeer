d118477e192bc2be73505375a31f69869009486a
WesleySchoutenNL <wesleyschouten@outlook.com>
2025-06-19 12:19:41 +0200

    Final git push voor het afronden van het semester 2

--- Festisfeer.Presentation/InputModels/FestivalInputModel.cs
+++ Festisfeer.Presentation/InputModels/FestivalInputModel.cs
@@ -8,8 +8,8 @@ namespace Festisfeer.Presentation.InputModels
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Location { get; set; }
-        public DateTime StartDateTime { get; set; }
-        public DateTime EndDateTime { get; set; }
+        public DateTime StartDateTime { get; set; } = DateTime.Now;
+        public DateTime EndDateTime { get; set; } = DateTime.Now;
         public string? Genre { get; set; }
         public int TicketPrice { get; set; }
         public IFormFile? FestivalImg { get; set; }
